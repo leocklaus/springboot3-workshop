a6b38195a455fb6f6e0ac2a62e16596c5337d54b
leocklaus <leocklaus@gmail.com>
2023-04-10 18:29:43 -0300

    Product

---
+++ src/main/java/com/lck/springcourse/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.lck.springcourse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lck.springcourse.entities.Product;
+import com.lck.springcourse.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository userRepository;
+	
+	public List<Product> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = userRepository.findById(id);
+		return obj.get();
+	}
+	
+}
