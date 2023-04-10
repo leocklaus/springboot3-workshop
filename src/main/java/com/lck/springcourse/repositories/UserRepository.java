bcf98aa70045ac07b3777406af73deffc978fef6
leocklaus <leocklaus@gmail.com>
2023-04-10 12:04:00 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/lck/springcourse/repositories/UserRepository.java
+++ src/main/java/com/lck/springcourse/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.lck.springcourse.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lck.springcourse.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
